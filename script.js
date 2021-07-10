6be4de7d2dd1455b499ca539c1b2ea22bfb1d1fb
Makktu <johnmcna@gmail.com>
2021-07-10 01:20:37 +0100

    Friday afternoon

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const canvas = document.querySelector(".canvas");
+
+const smallBtn = document.querySelector(".small");
+smallBtn.addEventListener("click", () => {
+    console.log("small");
+});
+
+const mediumBtn = document.querySelector(".medium");
+mediumBtn.addEventListener("click", () => {
+    console.log("medium");
+});
+
+const largeBtn = document.querySelector(".large");
+largeBtn.addEventListener("click", () => {
+    console.log("large");
+});
+
+const xlargeBtn = document.querySelector(".x-large");
+xlargeBtn.addEventListener("click", () => {
+    console.log("x-large");
+});
